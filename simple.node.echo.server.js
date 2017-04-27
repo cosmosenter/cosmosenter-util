f507656bf1a248d9403af54c3004dd74cafc4bdf
hellogom <novemberizing@gmail.com>
2017-04-27 18:26:36 +0900

    add node echo server & add simple websocket client

---
+++ simple.node.echo.server.js
@@ -0,0 +1,23 @@
+/**
+ * simple echo node js server
+ */
+
+var net = require("net");
+var StringDecoder = require('string_decoder').StringDecoder;
+var decoder = new StringDecoder("utf-8");
+var clients = [];
+
+var server = net.createServer(function(socket){
+    console.log("create");
+    clients.push(socket);
+    socket.on("data", function(data){
+        socket.write(data);
+    });
+    socket.on("end", function(){
+        clients.splice(clients.indexOf(socket), 1);
+        console.log("close");
+    });
+});
+
+server.listen(13371, "0.0.0.0");
+
